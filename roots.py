ac9a73ccd7b1559ebd6279fd3fe3e74ac2b851f7
yararossi <rossi.yara@outlook.com>
2024-08-26 15:54:52 -0700

    adding hualien roots.

--- roots.py
+++ roots.py
@@ -5,3 +5,9 @@ def get_roots():
     root_savefig = '/Users/yararossi/Documents/Work/Towards_Quantification/3_Projects/AttitudeEquation/Figures_coding/4SSA'
     root_processeddata = '/Users/yararossi/Documents/Work/Towards_Quantification/3_Projects/AttitudeEquation/Data/Processed'
     return root_originaldata, root_savefig, root_processeddata
+
+def get_rootsHualien():
+    root_originaldata = '/Users/yararossi/Documents/Work/Towards_Quantification/3_Projects/HUalienTilt_20240402/202404_20240402_eq'
+    root_savefig = '/Users/yararossi/Documents/Work/Towards_Quantification/3_Projects/AttitudeEquation/Figures_coding/4SSA'
+    root_processeddata = '/Users/yararossi/Documents/Work/Towards_Quantification/3_Projects/AttitudeEquation/Data/ProcessedHualien'
+    return root_originaldata, root_savefig, root_processeddata
